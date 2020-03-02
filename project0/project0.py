bc4f4884e98c15bfdd2502d09a71759339455319
Rajashekar-Veer <Rajashekar-Veer>
2020-03-02 23:51:58 +0000

    added setupfiles

--- project0/project0.py
+++ project0/project0.py
@@ -1,11 +1,10 @@
 import sqlite3
-from urllib import request
 import tempfile
 import PyPDF2 as pd
 import numpy as np
 import pandas as p
 import re
-
+from urllib import request
 
 temp = tempfile.TemporaryFile(suffix='temp.pdf')
 def fetchIncidents(url):
@@ -13,6 +12,7 @@ def fetchIncidents(url):
     response = request.urlopen(url)
     pdf = response.read()
     temp.write(pdf)
+    return temp
 
 def extractIncidents():
     #this function is used to extract content and store each record in a list of list.
@@ -27,7 +27,6 @@ def extractIncidents():
     data = re.sub('NORMAN POLICE DEPARTMENT\n','',dataStr)
     data = re.sub('Daily Incident Summary \(Public\)','',data)
     data = re.sub(' \n', ' ', data)
-    rx = r'\s+(?=\d{1}/\d{2}/\d{4} \d{2}:\d{2})'
     data = re.split(r'\s+(?=\d{1,2}/\d{1,2}/\d{4} \d{1,2}:\d{1,2})', data)
     for i in data:
         data = i.split('\n')
@@ -40,33 +39,34 @@ def extractIncidents():
 def createdb():
     #this function creates the db and also creates the table incidents
     try:
-        database = 'normanpd.db'
-        dbase = sqlite3.connect(database)
-        point = dbase.cursor()
-        dropQuery = 'DROP TABLE IF EXISTS incidents;'
-        createQuery = 'CREATE TABLE IF NOT EXISTS incidents (DataTime TEXT, incidentNumber TEXT, location TEXT, Nature TEXT, incidentORI TEXT);'
-        point.execute(dropQuery)
-        point.execute(createQuery)
-        dbase.commit()
+        database = 'policeDept.db'
+        db = sqlite3.connect(database)
+        point = db.cursor()
+        dropScript = 'DROP TABLE IF EXISTS incidents;'
+        createScript = 'CREATE TABLE IF NOT EXISTS incidents (DataTime TEXT, incidentNumber TEXT, location TEXT, Nature TEXT, incidentORI TEXT);'
+        point.execute(dropScript)
+        point.execute(createScript)
+        db.commit()
         return database
-    except Error as err:
-        print(err)
+    except sqlite3.Error as error:
+        print(error)
 
 
 def dbInsert(db, incidents):
     #this function inserts the extracted data into incidents table
-    dbase = sqlite3.connect(db)
-    point = dbase.cursor()
+    db = sqlite3.connect(db)
+    point = db.cursor()
     insertQuery = 'INSERT INTO incidents VALUES (?, ?, ?, ?, ?);'
     for i in range(len(incidents)):
         point.execute(insertQuery, incidents[i])
-        dbase.commit()
+        db.commit()
 
 def dbStatus(db):
     #this function is used to display the count of nature from the incidents table
-    dbase = sqlite3.connect(db)
-    point = dbase.cursor()
+    db = sqlite3.connect(db)
+    point = db.cursor()
     point.execute('SELECT nature||"|"||count(*) as nature FROM incidents group by nature order by nature;')
     myResult = point.fetchall()
     for row in myResult:
         print(row[0])
+    return myResult
---
+++ tests/test_project0.py
@@ -0,0 +1,35 @@
+import pytest
+import sqlite3
+import project0
+
+url = 'http://normanpd.normanok.gov/filebrowser_download/657/2020-02-27%20Daily%20Incident%20Summary.pdf'
+
+def test_fetchIncidents():
+    assert project0.fetchIncidents(url) is not None
+
+def test_extractIncidents():
+    data = project0.extractIncidents()
+    for i in data:
+        assert len(i) == 5
+
+def test_createdb():
+    databaseName = project0.createdb()
+    assert databaseName == 'policeDept.db'
+
+def test_dbInsert():
+    db = project0.createdb()
+    incidents = project0.extractIncidents()
+    project0.dbInsert(db, incidents)
+    dbase = sqlite3.connect(db)
+    point = dbase.cursor()
+    point.execute('select count(*) from incidents;')
+    count = point.fetchone()
+    assert count[0] == len(incidents)
+
+def test_dbStatus():
+    db = project0.createdb()
+    incidents = project0.extractIncidents()
+    project0.dbInsert(db, incidents)
+    records = project0.dbStatus(db)
+    assert records is not None
+
